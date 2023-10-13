bb6efef6711238c90e7751b86cb3fb7b97e66ecc
Vitaly Teroshyn <takura2012@gmail.com>
2023-10-13 16:07:58 +0300

    Werkzeug-2

--- main.py
+++ main.py
@@ -1656,4 +1656,4 @@ if __name__ == '__main__':
     #         db.create_all()
     #     except Exception as e:
     #         print(e)
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=False, port='5001')
